caa1efdf4b20f3dacfb22f0ba7effe7c69fb71d8
jacob-dt <107429249+jacob-dt@users.noreply.github.com>
2023-12-11 13:44:20 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return <h2>image placeholder</h2>;
+};
+const Title = () => {
+  return <h2>Title</h2>;
+};
+const Author = () => {
+  return <h4>Author</h4>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList />);
